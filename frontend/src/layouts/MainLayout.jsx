c818eccd6fca74152015235678837aa698469110
Matthew Maung <matthewmaung02@gmail.com>
2025-03-08 15:49:12 -0500

    added footer to the layout

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import MainHeader from '../components/MainHeader'
+import MainFooter from '../components/MainFooter'
 
 export default function MainLayout() {
   return (
     <>
       <MainHeader />
       <Outlet />
+      <MainFooter />
     </>
   )
 }
