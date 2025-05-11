c67789d6eab4c0d4558528e7bbca47c6b4b10643
Elton Salanic <elton.salanic@gmail.com>
2025-05-10 23:02:22 -0700

    database connection initialized

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,38 @@ import express from 'express';
 import pg from 'pg';
 import bcrypt from 'bcrypt';
 import dotenv from 'dotenv/config';
+import passport from 'passport';
+import session from 'express-session';
+import { Strategy } from 'passport-local';
 
 const app = express();
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
+});
+
+db.connect();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//test endpoint
+app.get('/test', async (req, res) => {
+  try {
+    console.log("Starting");
+    const results = await db.query('SELECT * FROM admin_users');
+    console.log(results.rows);
+    console.log('done');
+    res.status(200).send();
+  } catch (e) {
+    console.log(e);
+  }
+
+})
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log("server started on port: " + process.env.SERVER_PORT);
