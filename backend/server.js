8df32a908d9c54b500732bc188709845583f1760
Elton Salanic <elton.salanic@gmail.com>
2025-05-10 23:30:37 -0700

    create admin user endpoint created

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ const db = new pg.Client({
 
 db.connect();
 
+//error messages will be sent through the backend!
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,8 +33,27 @@ app.get('/test', async (req, res) => {
   } catch (e) {
     console.log(e);
   }
-
 })
+
+//create an admin user
+app.post('/admin/users', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const { rows: users } = await db.query('SELECT * FROM admin_users WHERE email = $1', [email]);
+
+    if (users.length > 0) { //if email already exists
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const storeResults = await db.query('INSERT INTO admin_users(email, password) VALUES ($1, $2)', [email, hashedPassword]);
+    return res.status(200).send();
+  } catch (e) {
+    console.log("Server error creating user");
+    return res.status(400).json({ message: "Server error creating user" });
+  }
+});
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log("server started on port: " + process.env.SERVER_PORT);
