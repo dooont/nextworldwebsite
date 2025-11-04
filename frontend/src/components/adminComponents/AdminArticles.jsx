2c63ee0d145e219571222a4aae2423c3d69f6d9b
Elton Salanic <elton.salanic@gmail.com>
2025-11-03 19:57:58 -0800

    AdminArticles shouldn't be using SingleArticle
    at least for now, maybe in the future ill look for a better way to reconcile both admin halves

--- frontend/src/components/adminComponents/AdminArticles.jsx
+++ frontend/src/components/adminComponents/AdminArticles.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useState } from 'react';
+import SingleArticle from '../SingleArticle';
 /*
 title
 source
@@ -81,23 +82,7 @@ export default function AdminArticles({ articles, handleRefresh }) {
         {submissionUnsuccessful && <p className="text-red-500">Article Not Created!</p>}
       </form>
       {articles.map((article) => (
-        <article key={article.id} className="bg-gray-900 p-6 rounded-lg shadow-lg flex flex-col">
-          <h3 className="text-2xl font-semibold mb-2">{article.title}</h3>
-          <p className="text-sm text-gray-400 mb-4">{article.source} • {new Intl.DateTimeFormat('en-US', {
-            month: 'long',
-            year: 'numeric',
-          }).format(new Date(article.date.split('T')[0]))}</p>
-          <p className="flex-1 oswald-400 mb-4">{article.description}</p>
-          <a
-            href={article.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mt-auto text-blue-400 hover:underline"
-          >
-            Read More
-          </a>
-          <button onClick={() => { handleDeleteArticle(article.id) }} className="bg-red-500 hover:bg-black transition">Delete</button>
-        </article>
+        <SingleArticle article={article} />
       ))}
     </div>
   )
