3ec17f524684d15d0d0726a4d4f70de731693814
Elton Salanic <elton.salanic@gmail.com>
2025-11-07 18:37:22 -0800

    added database pool

---
+++ backend/repositories/pool.js
@@ -0,0 +1,21 @@
+import pg from 'pg';
+import dotenv from 'config/dotenv';
+
+const { Pool } = pg;
+
+const dbPool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
+});
+
+dbPool.on('error', (err, client) => {
+  console.error('Unexpected error on idle database client: ', {
+    message: err.message,
+    stack: err.stack
+  });
+})
+
+export default dbPool;
