7c679669c6b1fb85383cc60dffb6db8340b194a2
Elton Salanic <elton.salanic@gmail.com>
2025-11-13 14:35:43 -0800

    moved inquiry email functionality to emailService.js

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,14 +1,18 @@
 import nodemailer from 'nodemailer';
 import EmailError from '../errors/EmailError.js';
 
-export async function sendPasswordResetRequestEmail(toEmail, resetUrl){
-  const transporter = nodemailer.createTransport({
+function createTransporter(){
+  return nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.FROM_EMAIL,
       pass: process.env.EMAIL_PASS
     }
   });
+}
+
+export async function sendPasswordResetRequestEmail(toEmail, resetUrl){
+  const transporter = createTransporter();
 
   const mailOptions = {
     from: `nxtworldcollective <${process.env.FROM_EMAIL}>`,
@@ -28,5 +32,28 @@ export async function sendPasswordResetRequestEmail(toEmail, resetUrl){
     await transporter.sendMail(mailOptions);
   }catch(err){
     throw new EmailError('Could not send reset email', 500);
+  }
+}
+
+export async function sendInquiryEmail(firstName, lastName, userEmail, inquiryBody){
+  const transporter = createTransporter();
+
+  const mailOptions = {
+    from: `Next World Collective <${process.env.FROM_EMAIL}>`,
+    to: process.env.FROM_EMAIL,
+    subject: "New NextWorld Inquiry",
+    html: /* html */`
+      <h3>You have recieved a new inqury from: </h3>
+      <p>${firstName} ${lastName}</p>
+      <p>${userEmail}</p>
+      <h3>Message: </h3>
+      <p>${inquiryBody}</p>
+    `
+  };
+
+  try{
+    await transporter.sendMail(mailOptions);
+  }catch(err){
+    throw new EmailError('Could not send inquiry email', 500);
   }
 }
--- backend/services/inquiriesService.js
+++ backend/services/inquiriesService.js
@@ -1,31 +1,5 @@
-import nodemailer from 'nodemailer';
-import { AppError } from "../errors/AppError.js";
+import { sendInquiryEmail } from "./emailService.js";
 
 export async function submitInquiry(firstName, lastName, userEmail, inquiryBody){
-  try{
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.FROM_EMAIL,
-        pass: process.env.EMAIL_PASS
-      }
-    });
-
-    const mailOptions = {
-      from: `Next World Collective <${process.env.FROM_EMAIL}>`,
-      to: 'elton.salanic@gmail.com',
-      subject: "New NextWorld Inquiry",
-      html: /* html */`
-        <h3>You have recieved a new inqury from: </h3>
-        <p>${firstName} ${lastName}</p>
-        <p>${userEmail}</p>
-        <h3>Message: </h3>
-        <p>${inquiryBody}</p>
-      `
-    };
-
-    await transporter.sendMail(mailOptions);
-  }catch(err){
-    throw new AppError('Could not send inquiry email', 500);
-  }
+  await sendInquiryEmail(firstName, lastName, userEmail, inquiryBody);
 }
