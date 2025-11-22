2579c9f3652b193ca5ec67bd101c9389dd7be8e5
Elton Salanic <elton.salanic@gmail.com>
2025-11-21 21:07:37 -0800

    changed upcomingEvent flyer_url field to image

--- backend/services/upcomingEventsService.js
+++ backend/services/upcomingEventsService.js
@@ -1,17 +1,23 @@
 import { saveUpcomingEvent, updateUpcomingEvent, deleteUpcomingEventById, findAllUpcomingEvents } from "../repositories/upcomingEventsRepository.js";
 
-export async function createUpcomingEvent(title, subtitle, url, image){
+export async function createUpcomingEvent(title, subtitle, url, image) {
   await saveUpcomingEvent(title, subtitle, url, image);
 }
 
-export async function editUpcomingEvent(id, title, subtitle, url, image){
+export async function editUpcomingEvent(id, title, subtitle, url, image) {
   return await updateUpcomingEvent(id, title, subtitle, url, image);
 }
 
-export async function removeUpcomingEvent(id){
+export async function removeUpcomingEvent(id) {
   await deleteUpcomingEventById(id);
 }
 
-export async function getAllUpcomingEvents(){
-  return await findAllUpcomingEvents();
+export async function getAllUpcomingEvents() {
+  const events = await findAllUpcomingEvents();
+  return events.map(event => {
+    return {
+      ...event,
+      image: event.flyer_url
+    }
+  })
 }
