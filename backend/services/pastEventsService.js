6e43ecc958d7492da237e182117a47f0152fc101
Elton Salanic <elton.salanic@gmail.com>
2025-11-25 13:11:38 -0800

    changed past events dtos

--- backend/services/pastEventsService.js
+++ backend/services/pastEventsService.js
@@ -1,13 +1,13 @@
 import { savePastEventWithArtists, deletePastEventById, findAllPastEvents } from "../repositories/pastEventsRepository.js";
 
-export async function createPastEvent(flyer, title, subtitle, description, place, artists){
-  return await savePastEventWithArtists(flyer, title, subtitle, description, place, artists);
+export async function createPastEvent(flyerUrl, title, date, description, place, artists) {
+  return await savePastEventWithArtists(flyerUrl, title, date, description, place, artists);
 }
 
-export async function removePastEvent(id){
+export async function removePastEvent(id) {
   return await deletePastEventById(id);
 }
 
-export async function getAllPastEvents(){
+export async function getAllPastEvents() {
   return await findAllPastEvents();
 }
