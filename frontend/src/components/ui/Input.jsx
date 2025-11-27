a89b371c442e8364246383b4678782912d3db52f
Elton Salanic <elton.salanic@gmail.com>
2025-11-26 23:03:42 -0800

    added services to upload to s3 from frontend and generate presigned url from backend

--- frontend/src/components/ui/Input.jsx
+++ frontend/src/components/ui/Input.jsx
@@ -1,4 +1,4 @@
-export default function Input({label, type = 'text', ...props}) {
+export default function Input({ label, type = 'text', ...props }) {
   return (
     <div>
       <label
@@ -7,7 +7,7 @@ export default function Input({label, type = 'text', ...props}) {
       >
         {label}
       </label>
-      
+
       {type === 'textarea' ? (
         <textarea
           className="w-full px-4 py-3 bg-black border border-purple-900 rounded-lg text-white oswald-400 focus:outline-none focus:border-purple-700 focus:ring-1 focus:ring-purple-700/50 transition"
