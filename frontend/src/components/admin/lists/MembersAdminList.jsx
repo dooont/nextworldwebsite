8bb45a14e07eea0ddf181344bba979ee3da7d71e
Elton Salanic <elton.salanic@gmail.com>
2025-12-04 19:06:33 -0800

    feat: delete member functionality

--- frontend/src/components/admin/lists/MembersAdminList.jsx
+++ frontend/src/components/admin/lists/MembersAdminList.jsx
@@ -1,14 +1,16 @@
 import ItemsList from "../ItemsList.jsx"
 import useFetch from "../../../hooks/useFetch.jsx";
-import { getAllMembers } from "../../../services/membersService.js";
+import { deleteMemberById, getAllMembers } from "../../../services/membersService.js";
 import Loading from "../../ui/Loading.jsx";
 import ItemCard from "../ItemCard.jsx";
 import H3 from "../../ui/H3.jsx";
 import LoadingSpinner from "../../ui/LoadingSpinner.jsx";
 import ErrorMessage from "../../ui/ErrorMessage.jsx";
+import useDelete from "../../../hooks/useDelete.jsx";
 
 export default function MembersAdminList({ onEditClick }) {
   const { isPending: isMembersPending, isError: isMembersError, data: allMembers } = useFetch({ queryFn: getAllMembers, queryKey: ['members'], config: { staleTime: 10 * 60 * 1000 } });
+  const { mutate: deleteMember, isPending: isDeletePending, isError: isDeleteError } = useDelete({mutationFn: deleteMemberById, queryKey: ['members']})
 
   return (
     <ItemsList itemsName="Members">
@@ -16,13 +18,13 @@ export default function MembersAdminList({ onEditClick }) {
         isMembersError ? <ErrorMessage>Could not get members</ErrorMessage> :
           allMembers.map((member) => {
             return (
-              <ItemCard key={member.id} onDelete={() => deletepastEvent(member.id)} onEdit={() => onEditClick(member)}>
+              <ItemCard key={member.id} onDelete={() => deleteMember(member.id)} onEdit={() => onEditClick(member)}>
                 <H3>{`${member.firstName} ${member.lastName}`}</H3>
               </ItemCard>
             )
           })}
-      {/*isDeletePending && <Loading />*/}
-      {/*isDeleteError && <ErrorMessage>Could not delete upcoming event</ErrorMessage>*/}
+      {isDeletePending && <Loading />}
+      {isDeleteError && <ErrorMessage>Could not delete member</ErrorMessage>}
     </ItemsList>
   )
 }
