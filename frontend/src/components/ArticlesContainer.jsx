721663aa483cb7a4b3e71070681a34da5d2f7cfd
Elton Salanic <elton.salanic@gmail.com>
2025-12-08 13:35:51 -0800

    refactor: articles fetched dynamically. split container into own component

---
+++ frontend/src/components/ArticlesContainer.jsx
@@ -0,0 +1,31 @@
+import useFetch from '../hooks/useFetch.jsx';
+import { getArticles } from '../services/articlesService.jsx';
+import ArticleCard from './ArticleCard.jsx';
+import Loading from './ui/Loading.jsx';
+import ErrorMessage from './ui/ErrorMessage.jsx';
+import InfoMessage from './ui/InfoMessage.jsx';
+
+export default function ArticlesContainer() {
+  const { isPending, isError, data: articles } = useFetch({ 
+    queryFn: getArticles, 
+    queryKey: ['articles'], 
+    config: { staleTime: 10 * 60 * 1000 } 
+  });
+
+  return(
+    <section className="py-20 bg-black text-white">
+      <div className="max-w-6xl mx-auto px-6">
+        <h2 className="text-5xl font-bold text-center racing-sans-one-regular mb-10">
+          Articles About Us
+        </h2>
+        {isPending ? <Loading item="Articles" /> :
+          isError ? <ErrorMessage>Could not load articles</ErrorMessage> :
+            articles.length === 0 ? <InfoMessage >There are currently no articles currently</InfoMessage> :
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {articles.map(article => <ArticleCard key={article.id} article={article}></ArticleCard>)}
+              </div>
+        }
+      </div>
+    </section>
+  )
+}
