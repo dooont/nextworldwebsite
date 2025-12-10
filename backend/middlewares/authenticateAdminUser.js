11c94ad9389d3a703610742f277018aca3452749
Elton Salanic <elton.salanic@gmail.com>
2025-12-10 12:40:03 -0800

    updated backend auth middleware error codes

--- backend/middlewares/authenticateAdminUser.js
+++ backend/middlewares/authenticateAdminUser.js
@@ -1,29 +1,32 @@
-import {jwtVerify, errors} from 'jose';
-import dotenv from 'dotenv/config'
-import AuthError from '../errors/AuthError.js';
+import { jwtVerify, errors } from "jose";
+import dotenv from "dotenv/config";
+import AuthError from "../errors/AuthError.js";
 
-
-export default async function authenticateAdminUser(req, res, next){
+export default async function authenticateAdminUser(req, res, next) {
   const authHeader = req.headers.authorization;
-  if(!authHeader){
-    throw new AuthError('No token found', 401);
+  if (!authHeader) {
+    throw new AuthError("No token found", 401);
   }
-  const token = authHeader.split(' ')[1];
-
+  const token = authHeader.split(" ")[1];
 
   const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-  try{
+
+  try {
     const shit = await jwtVerify(token, secret);
     next();
-  }catch(err){
-    if(err instanceof errors.JWTExpired){
-      throw new AuthError('Token expired', 401);
+  } catch (err) {
+    if (err instanceof errors.JWTExpired) {
+      throw new AuthError("Token expired", 401);
     }
 
-    if(err instanceof errors.JWSInvalid){
-      throw new AuthError('Token invalid', 401);
+    if (err instanceof errors.JWSInvalid) {
+      throw new AuthError("Token invalid", 403);
     }
-    
-    throw new AuthError('Authentication failed', 401)
+
+    if (err instanceof AuthError) {
+      throw err;
+    }
+
+    throw new AuthError("Authentication failed", 500, err);
   }
 }
