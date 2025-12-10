fa94d7b117886b35393051c326865e07b029ded0
Elton Salanic <elton.salanic@gmail.com>
2025-12-10 12:24:30 -0800

    frontend auth services created

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,29 @@
+import { api } from "../api/axios.js";
+
+/**
+ * Logs in an admin user
+ * @param {Object} credentials - The login credentials
+ * @param {string} credentials.email - Admin email
+ * @param {string} credentials.password - Admin password
+ * @returns {string} - Access Token
+ */
+export async function login(credentials) {
+  const response = await api.post('/auth/login', credentials);
+  return response.data.accessToken;
+}
+
+/**
+ * Logs out the current admin user
+ */
+export async function logout() {
+  await api.post('/auth/logout');
+}
+
+/**
+ * Retrieves a new access token. Requires cookie with accessToken
+ * @returns {string} refreshToken
+ */
+export async function refreshToken() {
+  const response = await api.post('/auth/refresh');
+  return response.accessToken;
+}
