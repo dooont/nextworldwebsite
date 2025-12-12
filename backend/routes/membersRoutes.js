adba4389fc9401f4dfe626ff55068bfa6333e5ea
Elton Salanic <elton.salanic@gmail.com>
2025-12-11 16:38:46 -0800

    added auth middleware to backend routes

--- backend/routes/membersRoutes.js
+++ backend/routes/membersRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { addMember, updateMember, deleteMember, getMembers, getEveryMember } from "../controllers/membersController.js";
-
+import authenticateAdminUser from "../middlewares/authenticateAdminUser.js";
 
 const membersRouter = express.Router();
 
-membersRouter.post('/', addMember);
+membersRouter.post('/', authenticateAdminUser, addMember);
 membersRouter.get('/', getEveryMember);
 membersRouter.get('/:type', getMembers);
-membersRouter.put('/:id', updateMember);
-membersRouter.delete('/:id', deleteMember);
+membersRouter.put('/:id', authenticateAdminUser, updateMember);
+membersRouter.delete('/:id', authenticateAdminUser, deleteMember);
 
 export default membersRouter;
 
--- backend/routes/upcomingEventsRoutes.js
+++ backend/routes/upcomingEventsRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { addUpcomingEvent, updateUpcomingEvent, deleteUpcomingEvent, getUpcomingEvents } from "../controllers/upcomingEventsController.js";
-
+import authenticateAdminUser from "../middlewares/authenticateAdminUser.js";
 
 const upcomingEventsRouter = express.Router();
 
-upcomingEventsRouter.post('/', addUpcomingEvent);
+upcomingEventsRouter.post('/', authenticateAdminUser, addUpcomingEvent);
 upcomingEventsRouter.get('/', getUpcomingEvents);
-upcomingEventsRouter.put('/:id', updateUpcomingEvent);
-upcomingEventsRouter.delete('/:id', deleteUpcomingEvent);
+upcomingEventsRouter.put('/:id', authenticateAdminUser, updateUpcomingEvent);
+upcomingEventsRouter.delete('/:id', authenticateAdminUser, deleteUpcomingEvent);
 
 export default upcomingEventsRouter;
